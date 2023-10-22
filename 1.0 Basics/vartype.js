ece2846fc89dce01278000898e72a1ad958faff2
Ashraful Kabir <ashrafulkabir2021@gmail.com>
2023-10-23 01:00:08 +0600

    vartype

---
+++ 1.0 Basics/vartype.js
@@ -0,0 +1,5 @@
+let x; //this is undefined
+let y=5; //this is number and type is number
+let z="hello" //this is string and type is string
+let c=true   // this is bolean and type is bolean
+let a=null  //this is null type
