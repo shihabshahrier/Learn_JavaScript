c2de1f84d0f630bcb854c5ea53b6a568c7398fc5
shihabshahrier <shahrier13@yahoo.com>
2023-10-23 01:53:20 +0600

    func 2

--- 1.0 Basics/functions.js
+++ 1.0 Basics/functions.js
@@ -1,4 +1,4 @@
-function multiplication(p, q) {
+function multiplication(p, q) { // returnable function
     return p * q;
 }
 let p, q, result;
@@ -6,3 +6,9 @@ p = 5;
 q = 6;
 result = multiplication(p, q);
 console.log(result);
+
+function printHello() { // non-returnable function
+    console.log("Hello");
+}
+printHello();
+
