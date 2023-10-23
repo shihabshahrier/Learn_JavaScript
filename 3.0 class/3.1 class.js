335c1eb2985b210ce48cf196479bfb917fa69ab3
shihabshahrier <shahrier13@yahoo.com>
2023-10-24 02:56:46 +0600

    update class 3.1

--- 3.0 class/3.1 class.js
+++ 3.0 class/3.1 class.js
@@ -1,16 +1,21 @@
-class Myclass{
-    constructor(name,age){
-        this.name=name
-        this.age=age
+class Myclass {
+    constructor(name, age) { // constructor
+        this.name = name
+        this.age = age
     }
-    printdetail(){
-        console.log("Name: ",this.name)
-        console.log("Age: ",this.age)
+    setName(name) { // method
+        this.name = name
+    }
+    printdetails() { // method
+        console.log("Name: ", this.name)
+        console.log("Age: ", this.age)
 
     }
 }
 
-const obj1=new Myclass("karim",18)
+const obj1 = new Myclass("karim", 18) // object creation 
 console.log(obj1.name)
 console.log(obj1.age)
-obj1.printdetail()
+obj1.printdetails()
+obj1.setName("Rahim")
+obj1.printdetails()
