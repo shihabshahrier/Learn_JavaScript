be4bb4ac21b9d3141b172d62da32a47913b82ab2
shihabshahrier <shahrier13@yahoo.com>
2023-10-24 02:54:30 +0600

    update function 2.1

--- 2.0 functions/2.1 type_of_function.js
+++ 2.0 functions/2.1 type_of_function.js
@@ -1,18 +1,25 @@
-function function1_multi( a, b){
+// 1. Function Declaration
+function function1_multi(a, b) {
     return a * b
 };
 
-//////////////////////
+// 2. Function Expression
+const function2_multi = function (a, b) { return a * b };
 
-const function2_multi = function (a ,b) {return a * b};
-
-////////////////////
-
+// 3. Function Constructor
 const function3_multi = new Function("a", "b", "return a * b");
-
 let x = function3_multi(4, 3);
 
-console.log(function1_multi(4,3))
-console.log(function2_multi(4,3))
-console.log(function3_multi(4,3))
+// 4. Arrow Function
+const function4_multi = (a, b) => { return a * b };
+
+// 5. Self Invoking Function
+(function () {
+    console.log("Hello! I called myself");
+})();
+
+console.log(function1_multi(4, 3));
+console.log(function2_multi(4, 3));
+console.log(function3_multi(4, 3));
+console.log(function4_multi(4, 3));
 
