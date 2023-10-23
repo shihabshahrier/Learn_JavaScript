49994230e55ac5fd54278f39b1c3b0ff9fcb1a3d
shihabshahrier <shahrier13@yahoo.com>
2023-10-23 15:45:03 +0600

    err_cur_for_loop1

--- 1.0 Basics/for_loop.js
+++ 1.0 Basics/for_loop.js
@@ -1,7 +1,7 @@
-for (let i=0; i<=10;){
-    if (i%2==0){
-        let x =i;
-        i++;
+for (let i = 0; i <= 10;) {
+    if (i % 2 == 0) {
+        console.log(i);
+
     }
+    i++;
 }
-console.log(x)
